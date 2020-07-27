7f11149bebbda3d0d67f3daa187214af44d2e889
guzmangustavo <39011999+guzmangustavo@users.noreply.github.com>
2020-07-26 21:25:11 -0300

    Upload files

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # StockMarket-Bot
+
 A Telegram chatbot that informs the last stock price in Argentinean stock markets.
+
+## Table of contents
+
+* [Introduction](#introduction)
+* [Supported stocks](#supportedstocks)
+* [Disclaimer](#disclaimer)
+
+### Introduction
+
+StockMarketAR is a Telegram chatbot that returns the last price in Argentinean market of the stock that you indicate. 
+
+In order to get this information, you must enter the stock's ticker.
+
+If you have a Telegram account, it is available at: [t.me/StockMarketARbot](https://telegram.me/StockMarketARbot).
+
+### Supported stocks
+
+The main Python library used in the development of StockMarketAR is [python-telegram-bot](https://python-telegram-bot.org/).
+
+As long as you provide the ticker, the bot can return the last price of the corresponding stock.
+
+The tickers supported by the chatbot are:
+
+ - AGRO, ALUA, AUSO.
+ - BBAR, BHIP, BMA, BOLT, BPAT, BRIO, BYMA.
+ - CADO, CAPU, CAPX, CARC, CECO2, CELU, CEPU, CGPA2, COME, CRES, CTIO, CVH.
+ - DGCU2, DOME, DYCA.
+ - EDN, ESME.
+ - FERR, FIPL.
+ - GAMI, GARO, GBAN, GCLA, GGAL, GRIM.
+ - MIRG.
+ - HARG, HAVA.
+ - INAG, INDU, INTR, INVJ, IRCP, IRSA.
+ - LEDE, LOMA, LONG.
+ - MERA, METR, MIRG, MOLA, MOLI, MORI.
+ - OEST.
+ - PAMP, PATA, PATY, PGR, POLL.
+ - RICH, RIGO, ROSE.
+ - SAMI, SEMI, SUPV.
+ - TECO2, TGLT, TGNO4, TGSU2, TRAN, TXAR.
+ - VALO.
+ - YPFD.
+
+The price is obtained by the use of [yfinance](https://pypi.org/project/yfinance/) library.
+
+### Disclaimer
+
+StockMarketAr is a software developed with a educational purpose. It wasn't designed to support real financial desicion making. So it shouldn't be used this way.
+
+Officials stock markets are the main source about stock prices.
+
+In Argentina, the enforcement authority in stock markets is the [National Value Comission](https://www.cnv.gov.ar/sitioweb/). In its site can be found more information.
---
+++ chatbot.py
@@ -0,0 +1,149 @@
+﻿#@author: Gustavo Guzmán
+import os
+from telegram.ext import Updater, MessageHandler, CommandHandler
+from telegram.ext import ConversationHandler, Filters
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+import yfinance as yf
+
+# Global variables
+token = os.environ["TOKEN"]
+keyboard = [['SI', 'NO']]
+reply_markup = ReplyKeyboardMarkup(keyboard,
+                                       one_time_keyboard = True,
+                                       resize_keyboard = True)
+PORT = int(os.environ.get("PORT", 5000))
+
+# Functions definition
+def start(update, context):
+    welcome_text = "Hola! Soy StockMarketAR. ¿Te interesa conocer la última cotización de alguna acción en los mercados de valores argentinos?"
+    update.message.reply_text(welcome_text, reply_markup = reply_markup)
+    return "CHOICE"
+
+def choice (update, context):
+    answer = update.message.text
+    user = update.message.from_user
+    update.message.reply_text("Gracias por tu respuesta!",
+                              reply_markup = ReplyKeyboardRemove())
+    if answer == 'SI':
+        update.message.reply_text("Por favor, indica el símbolo de la acción (ticker) cuya cotización te interesa conocer.")
+        return "STOCK"
+    else:
+        ending_text = "Por el momento, solo puedo brindarte la última cotización de acciones.\nVuelve pronto, estoy aprendiendo a realizar otras tareas."
+        update.message.reply_text(ending_text)
+        return ConversationHandler.END
+          
+def stock_query(update, context):
+    stock_name = update.message.text
+    stock_name = stock_name.upper()
+    accion = [
+        "AGRO",
+        "ALUA",
+        "AUSO",
+        "BBAR",
+        "BHIP",
+        "BMA",
+        "BOLT",
+        "BPAT",
+        "BRIO",
+        "BYMA",
+        "CADO",
+        "CAPU",
+        "CAPX",
+        "CARC",
+        "CECO2",
+        "CELU",
+        "CEPU",
+        "CGPA2",
+        "COME",
+        "CRES",
+        "CTIO",
+        "CVH",
+        "DGCU2",
+        "DOME",
+        "DYCA",
+        "EDN",
+        "ESME",
+        "FERR",
+        "FIPL",
+        "GAMI",
+        "GARO",
+        "GBAN",
+        "GCLA",
+        "GGAL",
+        "GRIM",
+        "MIRG",
+        "HARG",
+        "HAVA",
+        "INAG",
+        "INDU",
+        "INTR",
+        "INVJ",
+        "IRCP",
+        "IRSA",
+        "LEDE",
+        "LOMA",
+        "LONG",
+        "MERA",
+        "METR",
+        "MIRG",
+        "MOLA",
+        "MOLI",
+        "MORI",
+        "OEST",
+        "PAMP",
+        "PATA",
+        "PATY",
+        "PGR",
+        "POLL",
+        "RICH",
+        "RIGO",
+        "ROSE",
+        "SAMI",
+        "SEMI",
+        "SUPV",
+        "TECO2",
+        "TGLT",
+        "TGNO4",
+        "TGSU2",
+        "TRAN",
+        "TXAR",
+        "VALO",
+        "YPFD"
+        ]
+    if stock_name in accion:
+        price = yf.download(
+        f"{stock_name}"+".BA",
+        period = "1d"
+        )["Adj Close"][0]
+        update.message.reply_text(f"La última cotización de {stock_name} es "
+                              + f"{price:.2f}\n¿Deseas consultar otra cotización?",
+                              reply_markup = reply_markup)
+        return "CHOICE"
+    else:
+        update.message.reply_text(f"{stock_name} no parece ser un símbolo válido.\n¿Puedes ingresarlo otra vez?")
+        return "STOCK"
+        
+def cancel(update, context):
+     update.message.reply_text("Espero haberte ayudado. Hasta pronto!")
+     return ConversationHandler.END
+     
+def main():
+    updater = Updater(token, use_context = True)
+    disp = updater.dispatcher
+    conv_handler = ConversationHandler(
+        entry_points = [CommandHandler('start', start)],
+        states = {
+            "CHOICE": [MessageHandler(Filters.text, choice)],
+            "STOCK": [MessageHandler(Filters.text, stock_query)]
+            },
+        fallbacks = [CommandHandler('cancel', cancel)]
+        )
+    disp.add_handler(conv_handler)
+    updater.start_webhook(listen = "0.0.0.0",
+                          port = int(PORT),
+                          url_path = token)
+    updater.bot.setWebhook('https://appname.herokuapp.com/' + token)
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
